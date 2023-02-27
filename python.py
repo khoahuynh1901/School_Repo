663dc40008f64246b8f142ead4cd178053389b3c
Khoa Huynh <Khoahuynh1901@gmail.com>
2023-02-27 11:37:40 -0500

    i changed it

--- python.py
+++ python.py
@@ -1,17 +1,1 @@
-<<<<<<< HEAD
-=======
-print("khoa")
-
-def function ( apple, orange ,banana):
-	total = (apple + orange + banana) / 3
-	return total
-total_price = function(10,6,5)
-print(total_price)
-<<<<<<< HEAD
->>>>>>> master
-=======
-
-# this how to use function to calute stuff
-
-
->>>>>>> master
+# ->> start function here
