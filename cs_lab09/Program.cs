722bb50c6cec576ee052c3c4daa919328742fdab
Khoa Huynh <khoahuynh1901@gmail.com>
2023-03-09 11:58:02 -0500

    done

---
+++ cs_lab09/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace week_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of students: ");
+            int numStudents = int.Parse(Console.ReadLine());
+
+            student[] studentArray = new student[numStudents];
+
+            for (int i = 0; i < numStudents; i++)
+            {
+                Console.WriteLine("\nEnter information for student " + (i + 1));
+
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Mark: ");
+                double mark = double.Parse(Console.ReadLine());
+
+                studentArray[i] = new student(id, name, mark);
+            }
+
+            Console.WriteLine("\nStudent information:");
+            for (int i = 0; i < numStudents; i++)
+            {
+                Console.WriteLine("\nStudent " + (i + 1));
+                Console.WriteLine(studentArray[i]);
+            }
+
+        }
+    }
+}
---
+++ cs_lab09/student.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_09
+{
+    internal class student
+    {
+        private int id;
+        private string name;
+        private double mark;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; } 
+        }
+
+        public string Name
+        {
+            get { return name; }    
+            set { name = value; }   
+        }
+        public double Mark
+        {
+            get { return mark; }    
+            set { mark = value; }
+        }
+
+        public student(int Id, string Name, double Mark) //encapsulate for private attribute
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Mark = Mark;
+        }
+
+        public override string ToString()
+        {
+            return "ID: " + Id + "\nName: " + Name + "\nMark: " + Mark;
+        }
+    }
+}
